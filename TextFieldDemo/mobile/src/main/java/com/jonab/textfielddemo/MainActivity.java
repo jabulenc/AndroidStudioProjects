f33179962a3672081b6978e1d83b8ca923263449
Jon Abulencia <jon.abulencia@live.com>
2017-02-16 18:27:36 -0500

    Added image change

--- TextFieldDemo/mobile/src/main/java/com/jonab/textfielddemo/MainActivity.java
+++ TextFieldDemo/mobile/src/main/java/com/jonab/textfielddemo/MainActivity.java
@@ -5,21 +5,25 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    // This is for S2.L17-20
     EditText usernameField;
     EditText passwordField;
-    public void SendData(View view)
-    {
+    ImageView loginimage;
+    public void SendData(View view) {
         String username = usernameField.getText().toString();
         String password = passwordField.getText().toString();
         Boolean dataFilled = !(TextUtils.isEmpty(username) || TextUtils.isEmpty(password));
         if (!dataFilled)
             (Toast.makeText(getApplicationContext(), "Please Enter Info", Toast.LENGTH_SHORT)).show();
-        else
+        else {
             (Toast.makeText(getApplicationContext(), "GOT YA DEETS", Toast.LENGTH_SHORT)).show();
+            loginimage.setImageResource(R.drawable.hamburger);
+        }
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +31,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         usernameField = (EditText)findViewById(R.id.userNameField);
         passwordField = (EditText)findViewById(R.id.passwordField);
+        loginimage = (ImageView)findViewById(R.id.loginImage);
     }
 }
