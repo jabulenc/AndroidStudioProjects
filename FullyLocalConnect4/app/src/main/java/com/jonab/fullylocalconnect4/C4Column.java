d749bba26da9aa401022d11bfb4d6be18ba9113a
Jon Abulencia <jon.abulencia@live.com>
2017-08-20 10:15:45 -0400

    Made some more changes, added layout

--- FullyLocalConnect4/app/src/main/java/com/jonab/fullylocalconnect4/C4Column.java
+++ FullyLocalConnect4/app/src/main/java/com/jonab/fullylocalconnect4/C4Column.java
@@ -1,5 +1,10 @@
 package com.jonab.fullylocalconnect4;
 
+import android.annotation.TargetApi;
+import android.util.Log;
+import android.widget.LinearLayout;
+
+import java.util.List;
 import java.util.Stack;
 
 /**
@@ -8,5 +13,28 @@ import java.util.Stack;
 
 public class C4Column extends Stack<C4Piece>{
     private final int MAX_SIZE = 4;
+    private boolean isFull;
+    private LinearLayout columnLayout;
 
+    public boolean isFull() {
+        return this.size() == 4;
+    }
+
+    public C4Column() {
+
+    }
+    public C4Column(List<C4Piece> initialPieces, LinearLayout layout)
+    {
+        columnLayout = layout;
+        this.addAll(initialPieces);
+    }
+    public boolean addPiece(C4Piece newPiece)
+    {
+        if (this.isFull()){
+            Log.i("C4Column","Column was full");
+            return false;
+        }
+        this.push(newPiece);
+        return true;
+    }
 }
--- FullyLocalConnect4/app/src/main/java/com/jonab/fullylocalconnect4/Connect4.java
+++ FullyLocalConnect4/app/src/main/java/com/jonab/fullylocalconnect4/Connect4.java
@@ -2,15 +2,42 @@ package com.jonab.fullylocalconnect4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Connect4 extends AppCompatActivity {
 
-    private ArrayList<Array>
+    private Button turnButton;
+    private TextView gameStats;
+    private Color currentTurn;
+    private boolean gameIsRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect4);
+        currentTurn = Color.Leaf;
+        turnButton = (Button)findViewById(R.id.turnButton);
+        gameStats = (TextView) findViewById(R.id.gameStatus);
+        gameStats.setText("Press Start");
+        turnButton.setText("Start Game");
+        gameIsRunning = false;
     }
 
+    public void doEndTurn(View view){
+        if (!gameIsRunning){
+            gameIsRunning = true;
+            gameStats.setText("Hat's Turn");
+            turnButton.setText("End Turn");
+        }
+
+        if (currentTurn == Color.Leaf){
+            gameStats.setText("Hat's Turn");
+            currentTurn = Color.Hat;
+        } else {
+            gameStats.setText("Leaf's Turn");
+            currentTurn = Color.Leaf;
+        }
+    }
 }
